b2a59bef80c942d379c19c6ae505472dfc24f01d
ryzhan43 <ryzhan43@gmail.com>
2023-05-07 15:34:53 -0400

    activate bottom done

--- src/main/java/com/mryzhan/BankSimulationAppApplication.java
+++ src/main/java/com/mryzhan/BankSimulationAppApplication.java
@@ -17,20 +17,20 @@ public class BankSimulationAppApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
-
-        AccountService accountService = container.getBean(AccountService.class);
-        TransactionService transactionService = container.getBean(TransactionService.class);
-
-        Account sender = accountService.createNewAccount(BigDecimal.valueOf(78), new Date(), AccountType.CHECKING, 1L);
-        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.CHECKING, 2L);
-
-
-        accountService.listAllAccount().forEach(System.out::println);
-
-        transactionService.makeTransaction(sender, receiver, new BigDecimal(40), new Date(), "Transaction 1");
-
-        System.out.println(transactionService.findAllTransaction().get(0));
+      ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
+//
+//        AccountService accountService = container.getBean(AccountService.class);
+//        TransactionService transactionService = container.getBean(TransactionService.class);
+//
+//        Account sender = accountService.createNewAccount(BigDecimal.valueOf(78), new Date(), AccountType.CHECKING, 1L);
+//        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.CHECKING, 2L);
+//
+//
+//        accountService.listAllAccount().forEach(System.out::println);
+//
+//        transactionService.makeTransaction(sender, receiver, new BigDecimal(40), new Date(), "Transaction 1");
+//
+//        System.out.println(transactionService.findAllTransaction().get(0));
     }
 
 }

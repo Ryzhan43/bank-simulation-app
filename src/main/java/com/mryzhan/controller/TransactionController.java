403ded5d4e9d561bbac02e8d5d2cc58cc082dc5f
ryzhan43 <ryzhan43@gmail.com>
2023-08-23 19:01:43 -0400

    =Validetion added

--- src/main/java/com/mryzhan/controller/TransactionController.java
+++ src/main/java/com/mryzhan/controller/TransactionController.java
@@ -5,11 +5,13 @@ import com.mryzhan.service.AccountService;
 import com.mryzhan.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.UUID;
@@ -36,7 +38,14 @@ public class TransactionController {
     }
 
     @PostMapping("/make-transfer")
-    public String postMakeTransfer(@ModelAttribute("transaction") Transaction transaction){
+    public String postMakeTransfer(@Valid @ModelAttribute("transaction") Transaction transaction, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors())
+        {
+            model.addAttribute("accouint", accountService.listAllAccount());
+            return "transaction/make-transfer";
+        }
+
         System.out.println("transaction: "+ transaction.toString());
         transactionService.makeTransaction(
                 accountService.findById(transaction.getSender()),
